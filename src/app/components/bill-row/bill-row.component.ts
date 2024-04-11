187dfd30f138b5ab4f0b9c67dfc2c3bd60776c91
josemavarez11 <josemmr311@gmail.com>
2024-04-10 20:56:00 -0400

    bill labels and bill row components created

---
+++ src/app/components/bill-row/bill-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bill-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './bill-row.component.html',
+  styles: ``
+})
+export class BillRowComponent {
+
+}
